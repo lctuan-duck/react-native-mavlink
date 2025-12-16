42f1f7e0e1dc74a3f243137d3713dd34177a3f77
lctuan-duck <lctuan.dev@gmail.com>
2025-12-16 19:40:22 +0700

    feat: add base mavlink cpp

--- cpp/HybridMAVLink.hpp
+++ cpp/HybridMAVLink.hpp
@@ -3,6 +3,6 @@
 namespace margelo::nitro::math {
   class HybridMAVLink: public HybridMAVLink {
   public:
-    HybridMAVLink(): HybridObject(TAG) {}
+    HybridMAVLink(): HybridObject(TAG) {} // This constructor is required. TAG must have to fix errors.
   };
 }
